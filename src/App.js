31161c928a9f8ecdbce10e74e2006d8a283fbeec
alanboronat182 <alanboronat182@gmail.com>
2020-12-07 19:43:18 -0300

    primer commit
    carga inicial archivos del proyecto

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Buscador from './components/Buscador'
+
+
+class App extends Component {
+
+  datosBusqueda = (termino) => {
+    console.log(termino);
+  }
+
+  render() {
+    return (
+      <div className="app container">
+        <div className="jumbotron mt-2">
+          <p className="lead text-center">Buscador de imagenes</p>
+
+          <Buscador 
+            datosBusqueda={this.datosBusqueda}
+          />
+
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
