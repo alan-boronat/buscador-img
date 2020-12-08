cba211b994c01d857af72421f5f984765633e5fb
alanboronat182 <alanboronat182@gmail.com>
2020-12-07 21:17:33 -0300

    estructura principal
    form de busqueda, y consulta a la API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
-import Buscador from './components/Buscador'
+import Buscador from './components/Buscador';
+import Resultado from './components/Resultado';
 
 
 class App extends Component {
 
+  state = {
+    termino : '',
+    imagenes : []
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=19435393-b586ecc610c072877cf7c053b&q=${termino}`;
+
+    /* console.log(url); */
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({imagenes: resultado.hits }) )
+  }
+
   datosBusqueda = (termino) => {
-    console.log(termino);
+    this.setState({
+      termino
+    }, () => {
+      this.consultarApi();
+    })
   }
 
   render() {
@@ -19,6 +39,9 @@ class App extends Component {
           />
 
         </div>
+        <Resultado 
+          imagenes={this.state.imagenes}
+        />
       </div>
     );
   }
